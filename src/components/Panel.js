5d8c6e2afdeed1752deee939a31a5309f2b92656
Wendina <wendina.teng@gmail.com>
2023-04-29 17:47:54 -0400

    remove the id prop from the list of values we destructure from this.props. Remove the arrow function from the onClick event. Only pass the reference to the onSelect prop.

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 class Panel extends Component {
   render() {
-    const { id, label, value, onSelect } = this.props;
+    const { label, value, onSelect } = this.props;
 
     return (
-      <section onClick={event => onSelect(id)} className="dashboard__panel">
+      <section onClick={onSelect} className="dashboard__panel">
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
