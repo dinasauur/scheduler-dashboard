1e823f847c0e8ba0b718c55ee999bcf3f8499498
Wendina <wendina.teng@gmail.com>
2023-04-29 21:13:15 -0400

    clean up file. add webSocket connection to make the dashboard update in real time. add componentWillDismount to close socket.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,46 +8,46 @@ import {
   getTotalInterviews,
   getLeastPopularTimeSlot,
   getMostPopularDay,
-  getInterviewsPerDay
- } from "helpers/selectors";
+  getInterviewsPerDay,
+} from 'helpers/selectors';
 
 const data = [
   {
     id: 1,
-    label: "Total Interviews",
-    getValue: getTotalInterviews
+    label: 'Total Interviews',
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
-    label: "Least Popular Time Slot",
-    getValue: getLeastPopularTimeSlot
+    label: 'Least Popular Time Slot',
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
-    label: "Most Popular Day",
-    getValue: getMostPopularDay
+    label: 'Most Popular Day',
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
-    label: "Interviews Per Day",
-    getValue: getInterviewsPerDay
-  }
+    label: 'Interviews Per Day',
+    getValue: getInterviewsPerDay,
+  },
 ];
 
 class Dashboard extends Component {
   // inital state
-  state = { 
+  state = {
     loading: true,
     focused: null,
     days: [],
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   };
 
   // We use the componentDidMount lifecycle method to check to see if there is saved focus state after we render the application the first time.
   // When the local storage contains state, we can set the state of the application to match.
   componentDidMount() {
-    const focused = JSON.parse(localStorage.getItem("focused"));
+    const focused = JSON.parse(localStorage.getItem('focused'));
 
     if (focused) {
       this.setState({ focused });
@@ -56,75 +56,65 @@ class Dashboard extends Component {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
+      axios.get('/api/interviewers'),
     ]).then(([days, appointments, interviewers]) => {
       this.setState({
         loading: false,
         days: days.data,
         appointments: appointments.data,
-        interviewers: interviewers.data
-      })
-    })
+        interviewers: interviewers.data,
+      });
+    });
+
+    // makes this dashboard update in realtime. We will add a WebSocket connection that can update the state when we book or cancel an interview.
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
   }
 
-  // We can use the componentDidUpdate lifecycle method to listen for changes to the state. These functions belong to the Dashboard class. 
-  // The componentDidUpdate lifecycle method has access to the props and state from the previous update. 
+  // We can use the componentDidUpdate lifecycle method to listen for changes to the state. These functions belong to the Dashboard class.
+  // The componentDidUpdate lifecycle method has access to the props and state from the previous update.
   // We compare them to the existing state, and if the values change, we write the value to localStorage.
   componentDidUpdate(previousProps, previousState) {
     if (previousState.focused !== this.state.focused) {
-      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+      localStorage.setItem('focused', JSON.stringify(this.state.focused));
     }
   }
 
-  // // Instance method the long way
-  // selectPanel(id) {
-  //   this.setState({
-  //     focused: id
-  //   });
+  // Close the socket using the instance variable that holds the reference to the connection.
+  componentWillUnmount() {
+    this.socket.close();
+  }
 
-  //   if (this.state.focused === id) {
-  //     this.setState({
-  //       focused: null
-  //     })
-  //   }
-  // }
-
-   // Instance method the short way
-   selectPanel(id) {
-    this.setState(previousState => ({
-      focused: previousState.focused !== null ? null : id
+  // Instance method
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
     }));
   }
 
-  // /* Class Property with Arrow Function */
-  // selectPanel = id => {
-  //   this.setState({
-  //    focused: id
-  //   });
-  // };
-
   render() {
-
     const dashboardClasses = classnames('dashboard', {
-      'dashboard--focused': this.state.focused
+      'dashboard--focused': this.state.focused,
     });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = (this.state.focused ? data.filter( panel => this.state.focused === panel.id) : data)
-    .map(panel => (
-      <Panel 
-        key={panel.id} 
-        id={panel.id} 
-        label={panel.label} 
+    const panels = (
+      this.state.focused
+        ? data.filter((panel) => this.state.focused === panel.id)
+        : data
+    ).map((panel) => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
         value={panel.getValue(this.state)}
         onSelect={(event) => this.selectPanel(panel.id)}
       />
     ));
 
-    return (<main className={dashboardClasses}>{panels}</main>)
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
